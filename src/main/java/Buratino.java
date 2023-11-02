8487ffcfb9f2d7b7650295a420ca3116e0985472
Olga Surnina <veehja88@gmail.com>
2023-11-02 09:37:49 +0300

    Initial commit

---
+++ src/main/java/Buratino.java
@@ -0,0 +1,11 @@
+public class Buratino {
+
+    public static void main(String[] args) {
+
+        byte minuendApples = 6;
+        byte subtrahendApples = 1;
+        int residualApples = (minuendApples - subtrahendApples);
+
+        System.out.println("У Буратино осталось: " + residualApples + " яблок");
+    }
+}
