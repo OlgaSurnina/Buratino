fed21459cbfa3edfffd1c89ec51b27051c40ad29
Olga Surnina <veehja88@gmail.com>
2023-11-02 15:37:25 +0300

    task done

--- src/main/java/Buratino.java
+++ src/main/java/Buratino.java
@@ -3,15 +3,12 @@ public class Buratino {
     public static void main(String[] args) {
 
         byte minuendApples = 6;
-
         System.out.println("У Буратино было " + minuendApples + " яблок");
 
         byte subtrahendApples = 1;
-
         System.out.println("Барабас отобрал у Буратино " + subtrahendApples);
 
         int residualApples = (minuendApples - subtrahendApples);
-
         System.out.println("У Буратино осталось: " + residualApples + " яблок");
     }
 }
